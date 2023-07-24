dfd49c17b93218cc9b04ac2a128c64e39acfa6f8
JulianaAstra <julianaas@yandex.ru>
2023-07-24 14:44:43 +0300

    refactor: use css.module

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app.tsx';
-import './pages/404-page/404-page.css';
 import { Setting } from './const.ts';
 
 const root = ReactDOM.createRoot(
--- src/pages/404-page/404-page.tsx
+++ src/pages/404-page/404-page.tsx
@@ -1,16 +1,17 @@
 import { Link } from 'react-router-dom';
 import { AppRoute } from '../../const';
 import { Helmet } from 'react-helmet-async';
+import styles from './404-page.module.css';
 
 function Page404(): JSX.Element {
   return (
-    <div className='page-404'>
+    <div className={styles.page404}>
       <Helmet>
         <title>404</title>
       </Helmet>
       <h1>404</h1>
       <h2>Oops! This page is not found</h2>
-      <Link to={AppRoute.Root}>To main page</Link>
+      <Link className={styles.link} to={AppRoute.Root}>To main page</Link>
     </div>
   );
 }
