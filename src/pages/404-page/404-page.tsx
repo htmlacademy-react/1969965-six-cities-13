e07f31c0a4b9bdea3aa28f9f20da06e2eb022667
JulianaAstra <julianaas@yandex.ru>
2023-08-01 11:39:37 +0300

    refactor: language grammar change

--- src/pages/404-page/404-page.tsx
+++ src/pages/404-page/404-page.tsx
@@ -10,7 +10,7 @@ function Page404(): JSX.Element {
         <title>404</title>
       </Helmet>
       <h1>404</h1>
-      <h2>Oops! This page is not found</h2>
+      <h2>Oops! This page was not found</h2>
       <Link className={styles.link} to={AppRoute.Root}>To main page</Link>
     </div>
   );
