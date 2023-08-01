382cbbfa631b1d381faf80f1523f7ae526679077
JulianaAstra <julianaas@yandex.ru>
2023-08-01 13:10:29 +0300

    feat: add mocks for comments + types

---
+++ src/types/comment.ts
@@ -0,0 +1,13 @@
+type User = {
+    name: string;
+    avatarUrl: string;
+    isPro: boolean;
+}
+
+export type Comment = {
+  id: string;
+  comment: string;
+  date: string;
+  rating: number;
+  user: User;
+}
