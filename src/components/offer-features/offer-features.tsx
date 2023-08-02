fccc40d5c357b05655d5fa92d879a9324afbc168
JulianaAstra <julianaas@yandex.ru>
2023-08-02 23:15:41 +0300

    feat: add props

--- src/components/offer-features/offer-features.tsx
+++ src/components/offer-features/offer-features.tsx
@@ -3,14 +3,15 @@ import { turnFirstCharToUppercase } from '../../pages/offer-page/utils';
 type OfferFeaturesProps = {
   type: string;
   maxAdults: number;
+  bedrooms: number;
 }
 
-function OfferFeatures({type, maxAdults}: OfferFeaturesProps): JSX.Element {
+function OfferFeatures({type, maxAdults, bedrooms}: OfferFeaturesProps): JSX.Element {
   return (
     <ul className="offer__features">
       <li className="offer__feature offer__feature--entire">{turnFirstCharToUppercase(type)}</li>
       <li className="offer__feature offer__feature--bedrooms">
-              3 Bedrooms
+        {bedrooms} Bedrooms
       </li>
       <li className="offer__feature offer__feature--adults">
               Max {maxAdults} adults
