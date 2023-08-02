4924d7d3177698067519b2e0c177d60d62d462ca
JulianaAstra <julianaas@yandex.ru>
2023-08-02 21:14:23 +0300

    refactor: connect the gallery to offer component

--- src/pages/offer-page/offer-page.tsx
+++ src/pages/offer-page/offer-page.tsx
@@ -6,6 +6,7 @@ import { AppRoute } from '../../const';
 import { countRating } from '../../components/card/utils';
 import { turnFirstCharToUppercase } from './utils';
 import { getFavoriteStyles } from '../../utils';
+import OfferGallery from '../../components/offer-gallery/offer-gallery';
 
 type OfferPageProps = {
   detailedOffers: OfferDetailed[];
@@ -66,53 +67,7 @@ function OfferPage({detailedOffers, offers} : OfferPageProps): JSX.Element {
         <section className="offer">
 
           <div className="offer__gallery-container container">
-            <div className="offer__gallery">
-
-              {/* в компонент */}
-              <div className="offer__image-wrapper">
-                <img
-                  className="offer__image"
-                  src="img/room.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-              <div className="offer__image-wrapper">
-                <img
-                  className="offer__image"
-                  src="img/apartment-01.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-              <div className="offer__image-wrapper">
-                <img
-                  className="offer__image"
-                  src="img/apartment-02.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-              <div className="offer__image-wrapper">
-                <img
-                  className="offer__image"
-                  src="img/apartment-03.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-              <div className="offer__image-wrapper">
-                <img
-                  className="offer__image"
-                  src="img/studio-01.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-              <div className="offer__image-wrapper">
-                <img
-                  className="offer__image"
-                  src="img/apartment-01.jpg"
-                  alt="Photo studio"
-                />
-              </div>
-
-            </div>
+            <OfferGallery images = {images}/>
           </div>
 
           <div className="offer__container container">
